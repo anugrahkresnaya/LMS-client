f1a49b628dd400ae448372ce7bd6428b37222c85
anugrahkresnaya <anugrahkresnaya@gmail.com>
2023-06-04 00:08:00 +0700

    refactor: add update handler for user

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,102 +1,187 @@
 'use client'
 import axios from 'axios'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import profileCover from "../../public/profile-cover.jpg"
-import photo from '../../public/png_dc_kaze-12.png'
+import { Context } from '@/context'
+import defaultPhoto from '../../public/default.jpg'
 
 export default function Profile() {
-  const [token, setToken] = useState(null)
-  const [idUser, setIdUser] = useState(null)
+  const [hidden, setHidden] = useState(true)
+  const [userData, setUserData] = useState([])
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [birthDay, setBirthDay] = useState("")
+  const [gender, setGender] = useState("")
+  const [phoneNumber, setPhoneNumber] = ("")
+  const [photoProfile, setPhotoProfile] = useState(null)
+
+  const {
+    state: { user },
+    dispatch
+  } = useContext(Context)
 
   useEffect(() => {
-    setToken(sessionStorage.getItem('Access Token'))
-    setIdUser(sessionStorage.getItem('idUser'))
+    getUser()
   }, [])
 
   const getUser = async () => {
     try {
-      await axios.get(`http://localhost:3001/user/${idUser}`, {
-        headers: {
-          "Authorization": `Bearer ${token}`
-        }
-      })
-      .then(res => console.log(res))
+      const { data } = await axios.get(`http://localhost:3001/user/${user.id}`)
+      console.log('user data', data.data);
+      setHidden(false)
+      setUserData(data.data)
     } catch (error) {
       console.log(error)
+      setHidden(true)
     }
   }
 
-  console.log(getUser())
+  const onImageUpload = (e) => {
+    let file = e.target.files[0]
+    setPhotoProfile(file)
+  }
 
-  const isLoggedIn = token
-  console.log(isLoggedIn)
-  return(
-    <div className="drawer drawer-mobile">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-        <div className='flex flex-row'>
-          <div className='pl-5 mr-5 bg-base-300 rounded-md mockup-window'>
-            <h1 className="my-5">General Information</h1>
-            <form action="" className='flex-col'>
-              <div className='flex flex-row mb-5'>
-                <div className='flex flex-col mr-5'>
-                  <label htmlFor="firstName">First Name</label>
-                  <input type="text" name="firstName" id="" className='input input-bordered input-primary w-96' />
+  const handleUpdate = async (e) => {
+    e.preventDefault()
+    
+    try {
+      const { data } = await axios.put(`http://localhost:3001/user/update/${user.id}`, {
+        firstName: firstName,
+        lastName: lastName,
+        dateOfBirth: birthDay,
+        gender: gender,
+        photoProfile: photoProfile
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${user.accessToken}`,
+          "Content-Type": "multipart/form-data"
+        }
+      },
+      {
+        withCredentials: true,
+        credentials: 'include'
+      })
+
+      dispatch({
+        type: "LOGIN",
+        payload: data,
+      })
+      window.localStorage.setItem("user", JSON.stringify(data))
+
+      // Swal.fire({
+      //   position: "top-end",
+      //   title: "Successfull!",
+      //   icon: "success",
+      //   text: "Sign in Successfull",
+      //   showConfirmButton: false,
+      //   timer: 2000,
+      // })
+      // router.push("/")
+    } catch (error) {
+      // const errMsg = error.response?.data.error?.message
+      // if(errMsg !== undefined) {
+      //   Swal.fire('error', errMsg, 'error')
+      // }
+      console.log("api error: ", error)
+    }
+  }
+
+  const renderUser = userData.map(data => {
+    const dateString = data.dateOfBirth;
+    const date = new Date(dateString);
+
+    const options = { day: "numeric", month: "long", year: "numeric" };
+    const formattedDate = date.toLocaleDateString("en-US", options);
+
+    console.log(formattedDate);
+    return (
+      <div key={data.id} className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+          <div className='flex flex-row'>
+            <div className='pl-5 mr-5 bg-base-300 rounded-md mockup-window'>
+              <h1 className="my-5">General Information</h1>
+              <form action="/profile" encType='multipart/form-data' method='post' className='flex-col' onSubmit={handleUpdate}>
+                <div className='flex flex-row mb-5'>
+                  <div className='flex flex-col mr-5'>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" name="firstName" value={firstName} onChange={(e) => {setFirstName(e.target.value)}} placeholder="Your first name" id="firstName" className='input input-bordered input-primary w-96' />
+                  </div>
+                  <div className='flex flex-col mr-5'>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" name="lastName" value={lastName} onChange={(e) => {setLastName(e.target.value)}} placeholder="Your last name"  id="lastName" className='input input-bordered input-primary w-96' />
+                  </div>
                 </div>
-                <div className='flex flex-col mr-5'>
-                  <label htmlFor="lastName">Last Name</label>
-                  <input type="text" name="lastName" id="" className='input input-bordered input-primary w-96' />
+                <div className='flex flex-row mb-5'>
+                  <div className='flex flex-col  mr-5'>
+                    <label htmlFor="birthday">Birthday</label>
+                    <input type="date" name="birthday" value={birthDay}  id="" onChange={(e) => {setBirthDay(e.target.value)}} className='input input-bordered input-primary w-96' />
+                  </div>
+                  <div className='flex flex-col mr-5'>
+                    <label htmlFor="gender">Gender</label>
+                    <select className="select select-primary w-96" value={gender} onChange={(e) => {setGender(e.target.value)}}>
+                      <option disabled selected>Select your gender</option>
+                      <option>Male</option>
+                      <option>Female</option>
+                    </select>
+                  </div>
                 </div>
+                <div className='flex flex-row mb-5'>
+                  <div className='flex flex-col  mr-5'>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="" value={data.email} className='input input-bordered input-primary w-96' disabled />
+                  </div>
+                  <div className='flex flex-col  mr-5'>
+                    <label htmlFor="phone">Mobile Number</label>
+                    <input type="text" name="phone" id="" placeholder='Your mobile number' className='input input-bordered input-primary w-96' />
+                  </div>
+                </div>
+                <div className='mb-5'>
+                  <input type="file" name="photoProfile" onChange={(e) => onImageUpload(e)} className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
+                </div>
+                <button type="submit" className="btn btn-primary mb-5">Save All</button>
+              </form>
+            </div>
+            <div className="card ml-20 w-96 bg-base-300">
+              <figure><Image width={600} height={200} src={profileCover} className='object-cover' alt="car!"/></figure>
+              <div className="card-body pt-0">
+                <div className="avatar justify-center -mt-[50px]">
+                  <div className="w-24 rounded-full ring ring-primary-focus ring-offset-base-100 ring-offset-2">
+                    <Image src={data.photoProfile || defaultPhoto} width={96} height={96} alt="profile" />
+                  </div>
+                </div>
+                <h2 className="card-title justify-center mt-5">{data.firstName || "Anonim"} {data.lastName || "User"}</h2>
+                <p className="text-center">{formattedDate}</p>
+                <p>{data.phoneNumber}</p>
               </div>
-              <div className='flex flex-row mb-5'>
-                <div className='flex flex-col  mr-5'>
-                  <label htmlFor="birthday">Birthday</label>
-                  <input type="date" name="birthday" id="" className='input input-bordered input-primary w-96' />
-                </div>
-                <div className='flex flex-col mr-5'>
-                  <label htmlFor="gender">Gender</label>
-                  <select className="select select-primary w-96">
-                    <option disabled selected>Select your gender</option>
-                    <option>Male</option>
-                    <option>Female</option>
-                  </select>
-                </div>
-              </div>
-              <div className='flex flex-row mb-5'>
-                <div className='flex flex-col  mr-5'>
-                  <label htmlFor="email">Email</label>
-                  <input type="text" name="email" id="" className='input input-bordered input-primary w-96' />
-                </div>
-                <div className='flex flex-col  mr-5'>
-                  <label htmlFor="phone">Phone Number</label>
-                  <input type="text" name="phone" id="" className='input input-bordered input-primary w-96' />
-                </div>
-              </div>
-              <button className="btn btn-primary mb-5">Save All</button>
-            </form>
-          </div>
-          <div className="card ml-20 w-96 bg-base-300">
-            <figure><Image width={600} height={200} src={profileCover} className='object-cover' alt="car!"/></figure>
-            <div className="card-body pt-0">
-              <div className="avatar justify-center -mt-[50px]">
-                <div className="w-24 rounded-full ring ring-primary-focus ring-offset-base-100 ring-offset-2">
-                  <Image src={photo} width={96} height={96} alt="profile" />
-                </div>
-              </div>
-              <h2 className="card-title justify-center mt-5">Anugrah Kresnaya</h2>
             </div>
           </div>
+        </div> 
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+          </ul>
         </div>
-      </div> 
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-        <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-          <li><a>Sidebar Item 1</a></li>
-          <li><a>Sidebar Item 2</a></li>
-        </ul>
       </div>
-    </div>
+    )
+  })
+
+  return(
+    <>
+      {!hidden ? (
+        <div>
+          {renderUser}
+        </div>
+      ) : (
+        <div>
+          <h1>no user has login</h1>
+        </div>
+      )}
+    </>
   )
 }
